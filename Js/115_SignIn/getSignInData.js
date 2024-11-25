46b91181f026d94cbb2877a6930c772ed21731ed
ChenYue <chenyuem@Outlook.com>
2024-11-25 14:15:44 +0800

    add:115 sign in script

--- Js/115_SignIn/getSignInData.js
+++ Js/115_SignIn/getSignInData.js
@@ -6,6 +6,7 @@ function getSignInData() {
     bodyToken = $response.body
     $persistentStore.write(Cookie,'115PanCookie')
     $persistentStore.write(bodyToken,'115PanBody')
+    $notification.post('115CheckInScript', '获取签到数据成功！', '');
 }$done()
 
 getSignInData()

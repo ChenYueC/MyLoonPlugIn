4f68bf4dcbf64cd91156f12ac367b74167f30ed7
ChenYue <chenyuem@Outlook.com>
2024-11-28 08:45:44 +0800

    fix:update request

--- Js/115_SignIn/getSignInData.js
+++ Js/115_SignIn/getSignInData.js
@@ -2,8 +2,8 @@ let Cookie
 let bodyToken
 // =  $persistentStore.read("115PanCookie")
 function getSignInData() {
-    Cookie = $response.headers
-    bodyToken = $response.body
+    Cookie = $request.headers
+    bodyToken = $request.body
     $persistentStore.write(JSON.stringify(Cookie),'115PanCookie')
     $persistentStore.write(bodyToken,'115PanBody')
     $notification.post('115CheckInScript', '获取签到数据成功！', '');
